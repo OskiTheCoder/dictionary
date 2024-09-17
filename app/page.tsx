bcfa0169a31c77b0fe9b618adcb4bee348a0460d
shelvy <sks91@berkeley.edu>
2024-09-16 20:34:47 -0700

    basic handling for select

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+'use client';
+import { useState } from 'react';
 import Header from '@/components/header';
 import Main from '@/components/main';
 
 export default function Home() {
+  const [fontFamily, setFontFamily] = useState('font-mono');
+  const switchFont = (value: string) => setFontFamily(value);
   return (
-    <section className='w-full max-w-4xl mx-auto'>
-      <Header />
+    <section className={`w-full max-w-4xl mx-auto ${fontFamily}`}>
+      <Header onValueChange={switchFont} />
       <Main />
     </section>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -9,20 +9,24 @@ import {
 import { BookA } from 'lucide-react';
 import { ModeToggle } from './mode-toggle';
 
-export default function Header() {
+export default function Header({
+  onValueChange,
+}: {
+  onValueChange: (value: string) => void;
+}) {
   return (
     <header className='flex justify-between items-center p-8 w-full'>
       <BookA size={32} />
       <div className='flex justify-between items-center gap-4'>
-        <Select defaultValue='sans-serif'>
-          <SelectTrigger className='w-[120px]'>
+        <Select onValueChange={onValueChange} defaultValue='font-mono'>
+          <SelectTrigger className='w-[140px]'>
             <SelectValue />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectItem value='sans-serif'>Sans Serif</SelectItem>
-              <SelectItem value='monospace'>Monospace</SelectItem>
-              <SelectItem value='serif'>Serif</SelectItem>
+              <SelectItem value='font-mono'>Monospace</SelectItem>
+              <SelectItem value='font-sans'>Sans Serif</SelectItem>
+              <SelectItem value='font-serif'>Serif</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>{' '}
